ffaa1835220879389c8bb1dc2bbb57cba1543b84
Sergii Antropov <homoantropos@gmail.com>
2024-03-10 15:17:28 +0200

    view controller manages scheme ui elements

--- js/scheme/services/viewController.js
+++ js/scheme/services/viewController.js
@@ -1,34 +1,104 @@
 class ViewController {
     initViewController() {
-        Object.keys(this.elementsMap).map(
+        this.schemeUiElementsKeys.map(
             key => {
-                let element = this.getElementFromMap(key);
-                let { elementClass, targetElement, listeners } = element;
-                targetElement = document.getElementsByClassName(elementClass)[0];
-                element.targetElement = targetElement;
+                let element = this.getSchemeUiElement(key);
+                let {elementClass, listeners} = element;
+                element.targetElement = document.getElementsByClassName(elementClass)[0];
                 listeners.forEach(
-                    listener => targetElement && targetElement.addEventListener(listener.eventName, listener.callback)
+                    listener => {
+                        if (element.targetElement) {
+                            element.targetElement.addEventListener(listener.eventName, listener.callback);
+                            this.setElementVisibility(key, element.hide);
+                        }
+                    }
                 )
             }
         )
     }
-    getElementFromMap(elementName) {
-        return this.elementsMap[elementName];
+    // elements visibility
+    setElementVisibility(schemeElementUiName, hide) {
+        const schemeUiElement = this.getSchemeUiElement(schemeElementUiName);
+        if(!schemeUiElement.targetElement) {
+            console.log('Target element is empty!');
+            return;
+        }
+        schemeUiElement.hide = hide;
+        if (!schemeUiElement.elementClass.includes('button')) {
+            schemeUiElement.targetElement.hidden = schemeUiElement.hide;
+        } else {
+            schemeUiElement.targetElement.style.display = schemeUiElement.hide ? 'none' : 'block';
+        }
     }
-    closeElement(elementName) {
-        const { targetElement } = this.getElementFromMap(elementName);
-        targetElement && (targetElement.hidden = true);
+    showElement(schemeElementUiName) {
+        this.setElementVisibility(schemeElementUiName, false);
     }
-    elementsMap = {
+    hideElement(schemeElementUiName) {
+        this.setElementVisibility(schemeElementUiName, true);
+    }
+    showElements(namesElementsToShowArray) {
+        if (!Array.isArray(namesElementsToShowArray)) return;
+        const checkType = namesElementsToShowArray.every(el => typeof el === 'string');
+        if (!checkType) return;
+        this.schemeUiElementsKeys.map(
+            key => {
+                namesElementsToShowArray.includes(key) ? this.showElement(key) : this.hideElement(key);
+            }
+        );
+    }
+    // buttons disable attribute
+    setButtonDisabledState(buttonName, disableSate) {
+        const button = this.getSchemeUiElement(buttonName);
+        button.hide = disableSate;
+        button.disabled = disableSate;
+        button.targetElement && (button.targetElement.disabled = button.disabled);
+    }
+    enableButton(elementName) {
+        this.setButtonDisabledState(elementName, false)
+    }
+    disableButton(elementName) {
+        this.setButtonDisabledState(elementName, true)
+    }
+    enableAllButtons() {
+        this.schemeButtons.forEach(
+            elementName => this.enableButton(elementName)
+        )
+    }
+
+    disableAllButtons() {
+        this.schemeButtons.forEach(
+            elementName => this.disableButton(elementName)
+        )
+    }
+
+    // scheme dom elements and config
+    getSchemeUiElement(elementName) {
+        return this.schemeUiElements[elementName];
+    }
+
+    get schemeUiElementsKeys() {
+        return Object.keys(this.schemeUiElements);
+    }
+
+    get schemeButtons() {
+        return this.schemeUiElementsKeys.filter(key => key.includes('Button'))
+    }
+
+    get schemeDivs() {
+        this.schemeUiElementsKeys.filter(key => !key.includes('Button'))
+    }
+
+    schemeUiElements = Object.seal({
         schemeWrapper: {
             elementClass: 'scheme__wrapper',
             targetElement: undefined,
+            hide: false,
+            elements: [],
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.schemeWrapper.targetElement)
                     }
                 }
             ]
@@ -36,12 +106,13 @@ class ViewController {
         mapContainer: {
             elementClass: 'map__container',
             targetElement: undefined,
+            hide: false,
+            elements: ['schemeWrapper', 'mapContainer', 'takeScreenButton', 'closeMapButton'],
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.mapContainer.targetElement);
                     }
                 }
             ]
@@ -49,12 +120,13 @@ class ViewController {
         previewContainer: {
             elementClass: 'preview__container',
             targetElement: undefined,
+            hide: true,
+            elements: ['schemeWrapper', 'previewContainer', 'editButton', 'closePreviewButton'],
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.previewContainer.targetElement);
                     }
                 }
             ]
@@ -62,12 +134,13 @@ class ViewController {
         canvasContainer: {
             elementClass: 'canvas__container',
             targetElement: undefined,
+            hide: true,
+            elements: ['schemeWrapper', 'canvasContainer', 'saveButton', 'canselButton'],
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.canvasContainer.targetElement);
                     }
                 }
             ]
@@ -75,12 +148,13 @@ class ViewController {
         takeScreenButton: {
             elementClass: 'take__screen__button',
             targetElement: undefined,
+            hide: false,
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.takeScreenButton.targetElement);
+                        this.showElements(this.getSchemeUiElement('previewContainer').elements);
                     }
                 }
             ]
@@ -88,12 +162,14 @@ class ViewController {
         saveButton: {
             elementClass: 'save__button',
             targetElement: undefined,
+            hide: true,
+            disabled: false,
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.saveButton.targetElement);
+                        this.showElements([]);
                     }
                 }
             ]
@@ -101,12 +177,14 @@ class ViewController {
         editButton: {
             elementClass: 'edit__button',
             targetElement: undefined,
+            hide: true,
+            disabled: false,
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.editButton.targetElement);
+                        this.showElements(this.getSchemeUiElement('canvasContainer').elements);
                     }
                 }
             ]
@@ -114,12 +192,13 @@ class ViewController {
         restoreButton: {
             elementClass: 'restore__button',
             targetElement: undefined,
+            hide: true,
+            disabled: false,
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.restoreButton.targetElement);
                     }
                 }
             ]
@@ -127,25 +206,44 @@ class ViewController {
         canselButton: {
             elementClass: 'cansel__button',
             targetElement: undefined,
+            hide: true,
+            disabled: false,
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.canselButton.targetElement);
+                        this.showElements(this.getSchemeUiElement('previewContainer').elements);
                     }
                 }
             ]
         },
-        closeButton: {
-            elementClass: 'close__button',
+        closePreviewButton: {
+            elementClass: 'close__preview__button',
             targetElement: undefined,
+            hide: true,
+            disabled: false,
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        this.closeElement('schemeWrapper');
+                        this.showElements(this.getSchemeUiElement('mapContainer').elements);
+                    }
+                }
+            ]
+        },
+        closeMapButton: {
+            elementClass: 'close__map__button',
+            targetElement: undefined,
+            hide: false,
+            disabled: false,
+            listeners: [
+                {
+                    eventName: 'click',
+                    callback: ($event) => {
+                        $event.stopPropagation();
+                        this.showElements([]);
                     }
                 }
             ]
@@ -153,17 +251,19 @@ class ViewController {
         deleteButton: {
             elementClass: 'delete__button',
             targetElement: undefined,
+            hide: true,
+            disabled: false,
             listeners: [
                 {
                     eventName: 'click',
                     callback: ($event) => {
                         $event.stopPropagation();
-                        console.log('Class: ', this.elementsMap.deleteButton.targetElement);
+                        this.showElements(['schemeWrapper', 'mapContainer', 'takeScreenButton', 'closeMapButton']);
                     }
                 }
             ]
         }
-    }
+    })
 }
 
-export const viewController =  new ViewController();
+export const viewController = new ViewController();
