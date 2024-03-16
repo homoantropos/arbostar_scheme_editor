c1d1d698aa243bc01b4e51a68be0b534081931ca
Sergii Antropov <homoantropos@gmail.com>
2024-03-16 11:51:10 +0200

    toggle editor animation added

--- js/entryGate/http/requests.js
+++ js/entryGate/http/requests.js
@@ -1,6 +1,9 @@
 import {headers} from "./headers.js";
 import {imageExample} from "../../mockddata/imageexample.js";
 
+export async function saveCreatedScheme(scheme) {
+
+}
 export async function retrieve(url) {
     try {
         const response = await fetch(url, {
@@ -21,6 +24,13 @@ export async function retrieve(url) {
     }
 }
 
+export async function upldateScheme(schemePayload, bodyObj) {
+
+}
+
+export async function deleteScheme(schemeUrlOrID) {
+
+}
 export async function retrieveMockImage() {
     try {
         await new Promise(resolve => setTimeout(() => resolve(), 1000));
