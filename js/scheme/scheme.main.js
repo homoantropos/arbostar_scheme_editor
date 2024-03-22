570376b2dc0f885d48cce472f4b6997d24b8411f
Sergii Antropov <homoantropos@gmail.com>
2024-03-22 08:20:39 +0200

    to do list renewed

--- js/scheme/scheme.main.js
+++ js/scheme/scheme.main.js
@@ -22,9 +22,10 @@ export function initSchemeComponent() {
 LOGIC
 1. add figure marker to stickers
 2. add trash bin functionality
-
+3. add proper behavior of gallery tools icons - when fabric page is closed every should set as untouched
 VIEW
 1. approve proper view
-2. set input slider color along with paintColor prop
-
+2. [ schemeViewController - fabric ] set input slider color along with paintColor prop
+3. [ schemeViewController - fabric ] add new one loader to hide fabric renders while rotate and other transitions
+4. [ schemeViewController - preview ] set proper img's sizing while window resizes
  */
